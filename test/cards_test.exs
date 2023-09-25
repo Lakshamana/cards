4446726d265e6c8a8ea08ba7cf905ca99baaefbf
Lakshamana <lakshamana@protonmail.com>
2023-09-25 15:18:58 -0300

    added some tests

--- test/cards_test.exs
+++ test/cards_test.exs
@@ -6,4 +6,17 @@ defmodule CardsTest do
     deck = Cards.create_deck()
     assert deck == Enum.uniq(deck)
   end
+
+  test "create_deck creates 52 cards" do
+    deck = Cards.create_deck(52)
+    assert length(deck) == 52
+  end
+
+  test "contains?/2 returns true if the deck contains the card, otherwise false" do
+    :rand.seed(:exsplus, {1, 2, 3})
+    deck = Cards.create_deck()
+
+    assert Cards.contains?(deck, {"Clubs", "Seven"})
+    refute Cards.contains?(deck, {"Clubs", "King"})
+  end
 end
